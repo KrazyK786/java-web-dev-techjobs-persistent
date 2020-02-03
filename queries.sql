cc86b5b43145dd7f19e96c4205d7a1bde109bbb5
Kristopher Johnson <krazyk786@gmail.com>
2020-02-02 19:04:20 -0600

    implemented skill/employer repos, created SkillController, wired up repos in HomeController

--- queries.sql
+++ queries.sql
@@ -1,9 +1,13 @@
 ## Part 1: Test it with SQL
-id int
+id       int
 employer varchar(255)
 name     varchar(255)
 skills   varchar(255)
+
 ## Part 2: Test it with SQL
+SELECT *
+FROM techjobs.employer
+WHERE location = 'St. Louis City';
 
 ## Part 3: Test it with SQL
 
